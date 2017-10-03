c1b1b8b1902b08173643d5c81d232c7b70b0e649
alvar8 <alvarfernandez92@hotmail.com>
2017-10-03 13:14:51 +0200

    auth works

--- bullying/client/src/app/app.module.ts
+++ bullying/client/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+import { RouterModule } from '@angular/router';
+import { IsLoggedInService } from './services/is-logged-in.canactivate.service';
+import {routes} from './routes';
+import { HomeComponent } from './home/home.component';
+import { LoginformComponent } from './loginform/loginform.component';
+import { SignupformComponent } from './signupform/signupform.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginformComponent,
+    SignupformComponent,
+    UserprofileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService, IsLoggedInService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
