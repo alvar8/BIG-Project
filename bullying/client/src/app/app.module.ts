6478c81fca3193167d8dddf310a50e25f3a86dc5
alvar8 <alvarfernandez92@hotmail.com>
2017-10-07 14:54:38 +0200

    add boostrap

--- bullying/client/src/app/app.module.ts
+++ bullying/client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AuthService } from './services/auth.service';
@@ -20,6 +21,7 @@ import { EditprofileComponent } from './editprofile/editprofile.component';
 import { FileSelectDirective } from "ng2-file-upload";
 import { ChatComponent } from './chat/chat.component';
 import { SelfieComponent } from './selfie/selfie.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -32,13 +34,15 @@ import { SelfieComponent } from './selfie/selfie.component';
     EditprofileComponent,
     FileSelectDirective,
     ChatComponent,
-    SelfieComponent
+    SelfieComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AlertModule.forRoot()
   ],
   providers: [AuthService, IsLoggedInService,QuizzControllerService,MessageService,SelfieService],
   bootstrap: [AppComponent]
