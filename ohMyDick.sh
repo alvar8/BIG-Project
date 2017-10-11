64c16148ea7f7d5e03e6e846d7dff486081ac5f3
alvar8 <alvarfernandez92@hotmail.com>
2017-10-11 18:00:31 +0200

    deploy

--- ohMyDick.sh
+++ ohMyDick.sh
@@ -5,6 +5,8 @@ ng build -e prod
 cd ..
 echo "Copy files to public..."
 rm -rf ./server/public
+mkdir ./server/public
+mkdir ./server/public/uploads
 cp -r ./client/dist ./server/public
 
 git add .
